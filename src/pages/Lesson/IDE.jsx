e8e86fda8866b69cb2790ee5e42e88901fc85f22
Iacopo Schianchi <iacopo.schianchi@gmail.com>
2024-11-10 14:26:17 -0500

    Added HTML, CSS, and React editor options

--- src/pages/Lesson/IDE.jsx
+++ src/pages/Lesson/IDE.jsx
@@ -1,7 +1,81 @@
+import { useState, useEffect } from 'react';
 import './styles/IDE.css';
+import { Sandpack } from '@codesandbox/sandpack-react';
 
 export default function IDE({ languageID }) {
-    return (
+    const [width, setWidth] = useState(window.innerWidth);
+    let sandpackElement = (
+        <Sandpack
+            template="react"
+            theme="light"
+            options={{
+                showNavigator: true,
+                showLineNumbers: true,
+                showInlineErrors: true,
+                autoReload: true,
+                wrapContent: true,
+                resizablePanels: true,
+                editorHeight:
+                    width > 768
+                        ? 'calc(100vh - 149px)'
+                        : 'calc((100vh - 149px)/2)',
+            }}
+        />
+    );
+
+    if (languageID === 'html/css') {
+        sandpackElement = (
+            <Sandpack
+                template="static"
+                theme="light"
+                files={{
+                    '/index.html': {
+                        code: `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <!-- Enter your code here -->
+    </body>
+</html>
+                        `,
+                    },
+                    '/style.css': {
+                        code: ``,
+                    },
+                }}
+                options={{
+                    showNavigator: true,
+                    showLineNumbers: true,
+                    showInlineErrors: true,
+                    autoReload: true,
+                    wrapContent: true,
+                    resizablePanels: true,
+                    editorHeight:
+                        width > 768
+                            ? 'calc(100vh - 149px)'
+                            : 'calc((100vh - 149px)/2)',
+                }}
+            />
+        );
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', () => setWidth(window.innerWidth));
+
+        return () =>
+            window.removeEventListener('resize', () =>
+                setWidth(window.innerWidth)
+            );
+    }, []);
+
+    return languageID === 'html/css' || languageID === 'react' ? (
+        sandpackElement
+    ) : (
         <div className="ide-container">
             <iframe
                 src={`https://www.jdoodle.com/embed/v1/${languageID}`}
